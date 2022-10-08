5938aeeb43a2034f7f9ebc22ec7908774bbac084
Jobanpreet <jobanjatt113@gmail.com>
2022-10-08 10:18:10 +0530

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port= process.env.PORT || 3000;
 // const DB=process.env.DATABASE;
 
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect("mongodb+srv://joban:dlfnflN9485495LLNDFWwkldnd@cluster0.9ouvt.mongodb.net/natours?retryWrites=true&w=majority", {
     useNewUrlParser: true
   })
   .then(() => console.log('DB connection successful!'));
