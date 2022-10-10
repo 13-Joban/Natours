727413f20990e640250bb8f6aaf7fcf3174ac7d5
Jobanpreet <jobanjatt113@gmail.com>
2022-10-10 17:44:05 +0530

    changes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('https://hidden-fortress-89648.herokuapp.com/', viewRouter);
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
